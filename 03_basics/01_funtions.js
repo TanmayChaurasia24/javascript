f61424d16d0848ad983c90036c0557f14cf36d14
tanmay kumar chaurasia <130184741+TanmayChaurasia24@users.noreply.github.com>
2024-04-18 16:23:29 +0000

    functions in javascript

---
+++ 03_basics/01_funtions.js
@@ -0,0 +1,30 @@
+
+function saymyname() {
+    console.log("tanmay");
+}
+
+// saymyname()
+
+function add(num1,num2) {
+    return (num1 + num2)
+}
+// tanmay is default value
+function name(username = "tanmay") {
+    return `${username} is learning js`
+}
+const ans = add(3,3)
+// console.log(ans);
+console.log(name("tanmay kumar chaurasia")); // if value not passed then undefine will be taken
+
+
+
+
+
+
+
+
+
+
+
+
+
