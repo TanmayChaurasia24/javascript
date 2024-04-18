698d823f889cb5a9d748c5a58f3def626911faaf
tanmay kumar chaurasia <130184741+TanmayChaurasia24@users.noreply.github.com>
2024-04-18 10:53:54 +0000

    2: arrays in javascript

---
+++ 02_basic/02_arrays.js
@@ -0,0 +1,31 @@
+
+const arr1 = [1,23,4,5,65]
+const arr2 = [32,45,56,90]
+
+// arr1.push(arr2)
+// const newarray = arr1.concat(arr2) // returns new array
+// const newarray = [...arr1, ...arr2] // this is spread operator make array values seperate
+// const newarray = [12,[2,34,[3,56],[2,3,5]]]
+// const another = newarray.flat(Infinity) // makes all the values in one array
+
+console.log(Array.isArray("tanmay")); // tells it is an array or not
+console.log(Array.from("tanmay")); // makes array
+console.log(Array.from({name: "tanmay"})); // [] we have to tell to make aray of key or values
+console.log(Array.of(1,2,3)); // makes array of 1,2,3
+
+
+
+// console.log(another);
+
+
+
+
+
+
+
+
+
+
+
+
+
