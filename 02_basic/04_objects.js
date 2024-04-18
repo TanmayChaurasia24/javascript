843781cf4e5e665627f68db99d8c34429d355d07
tanmay kumar chaurasia <130184741+TanmayChaurasia24@users.noreply.github.com>
2024-04-18 15:41:12 +0000

    more on objects

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,34 @@
+// const tinder = new Object() // singleton object
+const tinder = {} // non sigleton object
+
+tinder.name = "tanmay"
+tinder.age = 21
+tinder.language = "javascript"
+
+const user = {
+    name: {
+        firstname: "tanmay",
+        lastname: "kumar"
+    },
+    age: 21
+}
+// console.log(user.name.firstname);
+
+const obj1 = {1:"a"}
+const obj2 = {2:"b"}
+
+// const obj3 = Object.assign({},obj1,obj2) // merge object
+const obj3 = {...obj1,...obj2}
+// console.log(obj3);
+
+
+
+
+
+
+
+
+
+
+
+
