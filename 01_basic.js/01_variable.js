fa042a5b2c67d76126159e8f0932b0f253747856
tanmay kumar chaurasia <130184741+TanmayChaurasia24@users.noreply.github.com>
2024-04-18 03:12:47 +0000

    second commit

---
+++ 01_basic.js/01_variable.js
@@ -0,0 +1,14 @@
+const accountid = 122333 // cannot change the const
+let accountemail = "tanmay@gmail.com" 
+var accountpass = "1111" // dont use it because of issue in functional blocks
+accountcity = "kanpur" // this is also possible but not prefer to use
+
+let name;
+
+name = "tanmay"
+accountemail = "sjdadkajsd"
+accountpass = "778878787"
+accountcity = "delhi"
+console.table([accountemail,accountid,accountpass,accountcity,name])
+
+
