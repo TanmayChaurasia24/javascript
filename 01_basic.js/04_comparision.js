1661b5275bd055c554fa4886ccc5f276a77228ed
tanmay kumar chaurasia <130184741+TanmayChaurasia24@users.noreply.github.com>
2024-04-18 04:46:10 +0000

    commit

---
+++ 01_basic.js/04_comparision.js
@@ -0,0 +1,13 @@
+/*
+    === => cheks for value and datatype 
+
+*/
+
+// console.log("2" > 1); // true
+// console.log("02" > 1); // true
+
+// similary for undefined all three are false
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); //true null is converted into 0
+
