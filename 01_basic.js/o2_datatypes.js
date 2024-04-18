76156b9379efad54683188975c52bc1283a30c6a
tanmay kumar chaurasia <130184741+TanmayChaurasia24@users.noreply.github.com>
2024-04-18 04:17:00 +0000

    commit

---
+++ 01_basic.js/o2_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all code as newer version of javascript
+
+// alert("be alert") 
+
+let name = "tanmay" // string
+let age = 10 // integer datatype
+let v = false // boolean 
+
+/*
+    number => 2 to power 32
+    bigint
+    string => ""
+    boolean => true/false
+    null => standalone value it means empty not undefine
+    undefine => value not assign
+    symbol => unique 
+
+*/
+
+console.log(typeof(12))
