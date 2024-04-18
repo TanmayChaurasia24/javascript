959777f62591e47d09271fc904c7a52c3a4c94c6
tanmay kumar chaurasia <130184741+TanmayChaurasia24@users.noreply.github.com>
2024-04-18 04:40:35 +0000

    commit

--- 01_basic.js/03_conversion.js
+++ 01_basic.js/03_conversion.js
@@ -13,18 +13,43 @@
 // console.table([num,typeof(num)])
 
 // let newnum = Number(num) // this will give NaN
-console.table([newnum,typeof(newnum)])
+// console.table([newnum,typeof(newnum)])
 
 
 // "33" => 33"
 // "33ac" => NaN
 // true => 1; false => 0 and vice versa
 
-let isloggedin = 1;
-let booleanisloggedin = Boolean(isloggedin)
-console.table([isloggedin,booleanisloggedin])
+// let isloggedin = 1;
+// let booleanisloggedin = Boolean(isloggedin)
+// console.table([isloggedin,booleanisloggedin])
 
 // to convert is string use String(num) 
+
+
+// -------------------operations------------------------------
+
+// 2**2 => 2 to the power 2
+
+// console.log("1" + 2) // 12
+// console.log("1" + 2 + 2) // 122
+// console.log(1 + 2 + "2") // 32
+
+console.log(+true); // 1
+// console.log(true+); // error
+
+// console.log(+""); // 0
+
+let y = 5;
+let result = ++y; // y is incremented to 6, then 6 is assigned to result
+console.log(result); // Output: 6
+console.log(y); // Output: 6
+
+
+let z = 5;
+let resultt = z++; // result is assigned 5 (current value of z), then z is incremented to 6
+console.log(resultt); // Output: 5
+console.log(z); // Output: 6
 
 
 
