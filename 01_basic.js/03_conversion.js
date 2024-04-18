732193c39591cd5b3bb9477e4dfae142105fe952
tanmay kumar chaurasia <130184741+TanmayChaurasia24@users.noreply.github.com>
2024-04-18 04:27:53 +0000

    commit

---
+++ 01_basic.js/03_conversion.js
@@ -0,0 +1,30 @@
+// let score = "32"
+// let score = null
+
+
+// console.log(typeof(score))
+
+// let s_score = Number(score)
+
+// console.log(typeof(s_score))
+
+
+// let num = "33and"
+// console.table([num,typeof(num)])
+
+// let newnum = Number(num) // this will give NaN
+console.table([newnum,typeof(newnum)])
+
+
+// "33" => 33"
+// "33ac" => NaN
+// true => 1; false => 0 and vice versa
+
+let isloggedin = 1;
+let booleanisloggedin = Boolean(isloggedin)
+console.table([isloggedin,booleanisloggedin])
+
+// to convert is string use String(num) 
+
+
+
