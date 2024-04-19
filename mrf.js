149753cf7660fdde49e1d107f076e76e34e1b29d
tanmay kumar chaurasia <130184741+TanmayChaurasia24@users.noreply.github.com>
2024-04-19 10:59:32 +0000

    js

---
+++ mrf.js
@@ -0,0 +1,22 @@
+const nums = [1,2,3,4,5,6,7,89]
+
+// nums.filter((num)=> {
+//     if(num > 4) {
+//         console.log(num);
+//     }
+// })
+
+// const newnums = nums.map((num) => {
+//     return num+10
+// })
+
+// console.log(newnums);
+
+
+
+
+
+
+
+
+
